9b202aa88b16eb5d04396d030dd949bcd11699be
thepsycopathlive <79912910+thepsycopathlive@users.noreply.github.com>
2021-12-14 07:59:14 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/NotZeetaa/Flashable_Zip_lmi.git -b alioth AnyKernel
 apt install sudo
 sudo apt install cpio
-apt install curl
+sudo apt install curl
 sudo apt update && sudo apt upgrade && sudo apt install gcc-aarch64-linux-gnu &&
 sudo apt install build-essential dkms linux-headers-$(uname -r) android-tools-adb android-tools-fastboot bc bison ca-certificates ccache clang cmake curl file flex gcc g++ git libelf-dev libssl-dev make ninja-build python3 texinfo u-boot-tools zlib1g-dev python vim repo
 apt install cpio
