84ff9deb501ec4d3ec9290f187b8630ac85723db
thepsycopathlive <79912910+thepsycopathlive@users.noreply.github.com>
2021-12-18 10:12:01 +0530

    Update kgsl_trace.c

--- drivers/gpu/msm/kgsl_trace.c
+++ drivers/gpu/msm/kgsl_trace.c
@@ -9,12 +9,6 @@
 
 /* Instantiate tracepoints */
 #define CREATE_TRACE_POINTS
-#include "kgsl_trace.h"
 #include "kgsl_trace_power.h"
-
-EXPORT_TRACEPOINT_SYMBOL(kgsl_regwrite);
-EXPORT_TRACEPOINT_SYMBOL(kgsl_issueibcmds);
-EXPORT_TRACEPOINT_SYMBOL(kgsl_user_pwrlevel_constraint);
-EXPORT_TRACEPOINT_SYMBOL(kgsl_constraint);
 
 EXPORT_TRACEPOINT_SYMBOL(gpu_frequency);
