7f6af7903c95cc1f3d3a1669e9514ed337a96871
thepsycopathlive <thepsycopathlive@gmail.com>
2021-12-19 12:20:09 +0000

    fix

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1,4 +1,4 @@
-/*
+ /*
  *  kernel/cpuset.c
  *
  *  Processor and Memory placement constraints for sets of tasks.
@@ -1547,12 +1547,9 @@ static void cpuset_attach(struct cgroup_taskset *tset)
 	struct cgroup_subsys_state *css;
 	struct cpuset *cs;
 	struct cpuset *oldcs = cpuset_attach_old_cs;
-	char name_buf[NAME_MAX + 1];
 
 	cgroup_taskset_first(tset, &css);
 	cs = css_cs(css);
-
-	cgroup_name(css->cgroup, name_buf, sizeof(name_buf));
 
 	mutex_lock(&cpuset_mutex);
 
@@ -1570,9 +1567,6 @@ static void cpuset_attach(struct cgroup_taskset *tset)
 		 * fail.  TODO: have a better way to handle failure here
 		 */
 		WARN_ON_ONCE(update_cpus_allowed(cs, task, cpus_attach));
-
-		if (!strncmp(name_buf, "background", 10))
-			set_user_nice(task, 10);
 
 		cpuset_change_task_nodemask(task, &cpuset_attach_nodemask_to);
 		cpuset_update_task_spread_flag(cs, task);
